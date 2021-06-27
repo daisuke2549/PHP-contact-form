75b068767e5a2fe9a376a512a88e3b8c1e3a9ac5
Daisuke Sasaki <gerund25495641@gmail.com>
2021-06-27 19:46:53 +0900

    sendmailを使ってお問い合わせフォームを作成

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
-<?php
-
-echo  'hello world';
-
-
-?>
+<html>
+<head>
+  <meta charset="utf-8" />
+</head>
+<body>
+<form action="./test_mail.php" method="post">
+		<p>受信者</p><input type="text" name="to">
+		<p>タイトル</p><input type="text" name="title">
+		<p>本文</p><textarea name="content" cols="50" rows="5"></textarea>
+		<p><input type="submit" name="send" value="送信"></p>
+		</form>
+</body>
+</html>
